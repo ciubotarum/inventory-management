f4dcfc825910924f9da36293a1e3c380aa40f0ef
ciubotarum <ciubotarum@outlook.com>
2024-07-08 10:56:45 +0300

    Adding Database Records

--- migrateSQLite3/worldtour/asiatoursagency/models.py
+++ migrateSQLite3/worldtour/asiatoursagency/models.py
@@ -11,3 +11,11 @@ class Tour(models.Model):
     destination_country = models.CharField(max_length=64)    
     number_of_nights = models.IntegerField()    
     price = models.IntegerField()    
+
+    # This is a String representation of the tours
+    def __str__(self):
+        return (
+            f"ID:{self.id}: From {self.origin_country} To "
+            f"{self.destination_country}, {self.number_of_nights} "
+            f"nights costs ${self.price}"
+        )
